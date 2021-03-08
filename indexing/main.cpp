95afe0e4f6c03813c7275565aec3db6dcb39ee26
Abuelanin <mabuelanin@gmail.com>
2021-03-08 20:30:32 +0200

    argv[1]

--- indexing/main.cpp
+++ indexing/main.cpp
@@ -6,7 +6,8 @@ int main(int argc, char ** argv){
     int kSize=31;
     int chunkSize = 100;
     // string genes_file = "/home/mabuelanin/dib-dev/kp_examples/indexing/data/gencode.v37.pc_transcripts.fa";
-    string genes_file = "/home/mabuelanin/dib-dev/kp_examples/indexing/data/gencode.v37.pc_transcripts.fa.split/gencode.v37.pc_transcripts.part_001.fa";
+    // string genes_file = "/home/mabuelanin/dib-dev/kp_examples/indexing/data/gencode.v37.pc_transcripts.fa.split/gencode.v37.pc_transcripts.part_001.fa";
+    string genes_file = argv[1];
     kDataFrame * genesFrame = new kDataFrameMQF(kSize);
     kmerDecoder * KMERS = kProcessor::initialize_kmerDecoder(genes_file, chunkSize, "kmers", {{"k_size", kSize}});
 
