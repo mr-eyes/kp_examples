636425250532a47428f1371fb8d0c5fb2507697d
Abuelanin <mabuelanin@gmail.com>
2021-03-08 20:19:17 +0200

    test_kp_index

---
+++ indexing/main.cpp
@@ -0,0 +1,23 @@
+#include "algorithms.hpp"
+
+int main(int argc, char ** argv){
+
+
+    int kSize=31;
+    int chunkSize = 100;
+    // string genes_file = "/home/mabuelanin/dib-dev/kp_examples/indexing/data/gencode.v37.pc_transcripts.fa";
+    string genes_file = "/home/mabuelanin/dib-dev/kp_examples/indexing/data/gencode.v37.pc_transcripts.fa.split/gencode.v37.pc_transcripts.part_001.fa";
+    kDataFrame * genesFrame = new kDataFrameMQF(kSize);
+    kmerDecoder * KMERS = kProcessor::initialize_kmerDecoder(genes_file, chunkSize, "kmers", {{"k_size", kSize}});
+
+    cerr << "Indexing started ..." << endl;
+    // kProcessor::index(KMERS, genes_file+".names", genesFrame);
+    kProcessor::index(genesFrame,{{"k_size", kSize}}, genes_file, chunkSize, genes_file+".names");
+
+
+    genesFrame->save("large_test");
+    cerr << "Indexing done ..." << endl;
+
+
+    return 0;
+}
